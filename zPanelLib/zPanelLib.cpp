686df830ce5c7422df294c2f3446b3df3776a9d0
zapline <278998871@qq.com>
2012-04-27 14:14:10 +0800

    + init zPanelLib

---
+++ zPanelLib/zPanelLib.cpp
@@ -0,0 +1,13 @@
+/********************************************************************
+    created:    2012.4.27  14:08
+    file path:  E:\zlib\zPanelLib\zPanelLib.cpp
+    author:     zapline<278998871@qq.com>
+*********************************************************************/
+
+#include "stdafx.h"
+#include "zPanelLib.h"
+
+void CCCA::Do()
+{
+    ::MessageBoxA(0, "1", "2", 0);
+}
---
+++ zPanelLib/zPanelLib.h
@@ -0,0 +1,14 @@
+/********************************************************************
+    created:    2012.4.27  13:58
+    file path:  E:\zlib\zPanelLib\zPanelLib.h
+    author:     zapline<278998871@qq.com>
+*********************************************************************/
+
+#pragma once
+
+class CCCA
+{
+public:
+    int a;
+    void Do();
+};
